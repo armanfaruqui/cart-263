1fa90a5a9ad37e0667363c8b12fa0c20cea2d11d
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2021-01-21 18:03:33 +0500

    Initialized repository and began sausage dog exercise

---
+++ exercises/template-p5-project/js/script.js
@@ -0,0 +1,27 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  createCanvas(500, 500);
+  background (0);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+fill(255,30,60);
+rectMode(CENTER);
+translate(100,100)
+rotate(60);
+rect(0,0,100,200);
+
+
+}
