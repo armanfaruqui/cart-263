e19f4d4381f07b0b523b9b85d65ac4e98ac2f331
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2021-01-22 17:57:40 +0500

    Began work on exercise 1

---
+++ exercises/exercise1 - sausageDog/js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage;
+let sausageDog;
+
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++){
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`)
+}
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < NUM_ANIMALS; i++){
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage)
+}
+
+function draw() {
+
+  background(34, 68, 32)
+
+  for (let i = 0; i < animals.length; i++){
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed(){
+  sausageDog.mousePressed()
+}
