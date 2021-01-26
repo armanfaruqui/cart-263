5bbcf9fb76f31c7e7465dedc5f0dfe44c09211dd
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2021-01-26 16:44:43 -0500

    Added a win screen and reset button

--- exercises/exercise1 - sausageDog/js/script.js
+++ exercises/exercise1 - sausageDog/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const NUM_ANIMAL_IMAGES = 10;
-const NUM_ANIMALS = 120;
+const NUM_ANIMALS = 110;
 
 let animalImages = [];
 let animals = [];
@@ -12,24 +12,26 @@ let sausageDogImage;
 let sausageDog;
 let titleScreen;
 let lossScreen;
+let winScreen;
 
 let state = "title" // State variable
 
 function preload() {
-  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++){
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
     let animalImage = loadImage(`assets/images/animal${i}.png`);
     animalImages.push(animalImage);
   }
   sausageDogImage = loadImage(`assets/images/sausage-dog.png`)
   titleScreen = loadImage(`assets/images/titleScreen.png`)
   lossScreen = loadImage(`assets/images/gordon.jpg`)
+  winScreen = loadImage(`assets/images/winScreen.jpg`)
 
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  for (let i = 0; i < NUM_ANIMALS; i++){
+  for (let i = 0; i < NUM_ANIMALS; i++) {
     let x = random(0, width);
     let y = random(0, height);
     let animalImage = random(animalImages);
@@ -46,66 +48,95 @@ function draw() {
 
   background(34, 68, 32)
 
+  gameLoss();
   displayRelevantScreen();
-  result();
+  restartButton()
 
   displayAnimals();
 
   displayLives();
 
   sausageDog.update();
+
+  console.log(state)
 }
 
-function mousePressed(){
+function mousePressed() {
   sausageDog.mousePressed()
-  if (state === "title"){
+  if (state === "title") {
     state = "game"
+  }
+
+  // Restarts game
+  if (state !== "title" && state !== "game" && mouseX > 30 && mouseX < 130 && mouseY > 30 && mouseY < 80) {
+    state = "game"
+    sausageDog.found = false
+    sausageDog.x = random(0, width)
+    sausageDog.y = random(0, height)
+    sausageDog.angle = 0
+    lives = 3
   }
 }
 
-function displayAnimals(){
-  if (state === "game"){
-  for (let i = 0; i < animals.length; i++){
-    animals[i].update();
+function displayAnimals() {
+  if (state === "game") {
+    for (let i = 0; i < animals.length; i++) {
+      animals[i].update();
     }
   }
 }
 
-function displayRelevantScreen(){
-  if (state === "title"){
+function displayRelevantScreen() {
+  if (state === "title") {
     push();
     image(titleScreen, 0, 0, width, height);
     pop();
   }
 
-  if (state === "loss"){
+  if (state === "loss") {
     push();
     imageMode(CENTER);
-    image(lossScreen, windowWidth/2, windowHeight/2);
+    image(lossScreen, windowWidth / 2, windowHeight / 2);
     pop();
   }
+  // Lets dog spin for 3 seconds before switching to the victory screen
+  if (sausageDog.found === true && state === "game") {
+    setTimeout(function(){state = "win"}, 3000);
+  }
 
-  if (state === "win"){
-    setTimeout()
+  if (state === "win") {
+    push();
+    imageMode(CENTER);
+    image(winScreen, windowWidth / 2, windowHeight / 2);
+    text("VICTORY", windowWidth / 2 - 75, windowHeight / 2 - 200)
+    pop();
   }
 }
 
-function displayLives(){
-  if (state === "game"){
-  textSize(24)
-  fill(255, 0, 0)
-  text(`${lives} Lives remaining`, 10, 30)
+// Displays live counter
+function displayLives() {
+  if (state === "game") {
+    textSize(24)
+    fill(255, 0, 0)
+    text(`${lives} Lives remaining`, 10, 30)
   }
 }
 
-function gameLoss(){
-  if (lives < 1){
+// Checks is user runs out of lives
+function gameLoss() {
+  if (state === "game" && lives < 1) {
     state = "loss"
   }
 }
 
-function gameWin(){
-  if (sausageDog.found === true){
-
+// Displays the restart button
+function restartButton() {
+  if (state === "win" || state === "loss") {
+    push()
+    fill(169, 237, 164)
+    rect(30, 30, 100, 50)
+    fill(233, 0, 0)
+    text("Restart", 40, 60)
+    pop()
   }
 }
