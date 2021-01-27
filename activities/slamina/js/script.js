bb7502ffe9f5d5f02a6b143831801b4e75fa41ac
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2021-01-27 12:41:45 -0500

    Began activity 2

---
+++ activities/slamina/js/script.js
@@ -0,0 +1,19 @@
+/**************************************************
+Template p5 project
+Pippin Barr
+
+Here is a description of this template p5 project.
+**************************************************/
+"use strict"
+function setup() {
+  createCanvas(500, 500);
+  background (0);
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+
+}
