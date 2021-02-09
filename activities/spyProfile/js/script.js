57f7df734c8669bba7452730a622cd059682890a
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2021-02-09 11:44:13 -0500

    Added option to change data

--- activities/spyProfile/js/script.js
+++ activities/spyProfile/js/script.js
@@ -11,12 +11,19 @@ let instrumentData = undefined;
 let objectData = undefined;
 let tarotData = undefined;
 
+//Position variables for reset buttons
+let resetButtons = {
+  x: 580,
+  y1: 164,
+  y2: 164 + 30,
+  y3: 164 + 60,
+}
+
 function preload() {
   tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`)
   instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`)
   objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`)
 }
-
 
 /**
 Description of setup
@@ -32,6 +39,8 @@ function setup() {
       spyProfile.alias = data.alias
       spyProfile.secretWeapon = data.secretWeapon
       spyProfile.password = data.password
+    } else if (password === `reset`) { //Allows the user to create a new account
+      generateSpyProfile()
     }
   } else {
     generateSpyProfile()
@@ -61,6 +70,10 @@ function draw() {
   Secret Weapon: ${spyProfile.secretWeapon}
   Password: ${spyProfile.password}`;
 
+  // console.log(`X${mouseX}`)
+  // console.log(`Y${mouseY}`)
+
+  displayResetButtons()
 
   push();
   textFont(`Courier, monospace`);
@@ -68,4 +81,32 @@ function draw() {
   textAlign(LEFT, TOP);
   text(profile, 100, 100)
   pop();
+}
+
+function mousePressed() {
+  if (mouseX > resetButtons.x - 8 && mouseX < resetButtons.x + 18 && mouseY > resetButtons.y1 - 3 && mouseY < resetButtons.y1 + 13) {
+    let instrument = random(instrumentData.instruments);
+    spyProfile.alias = `The ${instrument}`;
+    localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
+  }
+  if (mouseX > resetButtons.x - 8 && mouseX < resetButtons.x + 18 && mouseY > resetButtons.y2 - 3 && mouseY < resetButtons.y2 + 13) {
+    spyProfile.secretWeapon = random(objectData.objects);
+    localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
+  }
+  if (mouseX > resetButtons.x - 8 && mouseX < resetButtons.x + 18 && mouseY > resetButtons.y3 - 3 && mouseY < resetButtons.y3 + 13) {
+    let card = random(tarotData.tarot_interpretations);
+    spyProfile.password = random(card.keywords);
+    localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
+  }
+}
+
+// displays reset buttons
+function displayResetButtons() {
+  push()
+  fill(248, 0, 32)
+  noStroke()
+  triangle(resetButtons.x, resetButtons.y1, resetButtons.x, resetButtons.y1 + 20, resetButtons.x + 15, resetButtons.y1 + 10)
+  triangle(resetButtons.x, resetButtons.y2, resetButtons.x, resetButtons.y2 + 20, resetButtons.x + 15, resetButtons.y2 + 10)
+  triangle(resetButtons.x, resetButtons.y3, resetButtons.x, resetButtons.y3 + 20, resetButtons.x + 15, resetButtons.y3 + 10)
+  pop()
 }
