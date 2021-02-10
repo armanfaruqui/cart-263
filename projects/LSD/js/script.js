395079a03d454e1ef7514074c2e5c976298ebb90
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2021-02-10 18:41:01 -0500

    Worked on the intro

--- projects/LSD/js/script.js
+++ projects/LSD/js/script.js
@@ -1,18 +1,17 @@
 "use strict";
 
-/**
-Title of Project
-Author Name
+// State variable
+let state = "intro"
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+let startSimulation = false
 
-/**
-Description of preload
-*/
+let youtubeScreen;
+
+// Variables for the canvas size
+
+
 function preload() {
-
+  youtubeScreen = loadImage(`assets/images/youtube1.png`)
 }
 
 
@@ -20,7 +19,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 
@@ -28,5 +27,37 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(255, 255, 0)
 
+
+  displayIntro()
+  console.log(camera.position.x)
+
+}
+
+function mousePressed() {
+  // Begins simulation when user tries to play the video
+  if (state === "intro" && mouseX > 25 && mouseX < 913 && mouseY > 78 && mouseY < 574) {
+    startSimulation = true
+  }
+
+  console.log(mouseX) //25 913
+  console.log(mouseY) //78 574
+}
+
+function displayIntro() {
+  if (state === "intro") {
+    image(youtubeScreen, 0, 0, windowWidth, windowHeight)
+    if (startSimulation === true) {
+      if (camera.zoom < 1.8) {
+        camera.zoom = camera.zoom + 0.009
+      }
+      if (camera.position.x > 480) {
+        camera.position.x = camera.position.x - 2.3
+      }
+      if (camera.position.y > 320) {
+        camera.position.y = camera.position.y - 1
+      }
+    }
+  }
 }
